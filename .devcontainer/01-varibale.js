85819fbceafdd44e05a7c99ee85ae4a95dcb59c0
dhruvverma05 <dv1871889@gmail.com>
2025-09-13 19:15:01 +0530

    Update 01-varibale.js

--- .devcontainer/01-varibale.js
+++ .devcontainer/01-varibale.js
@@ -1,1 +1,7 @@
+const accountId =144553
+let accountEmail = "jdhdynhfh"
+var accountPassword = "2345"
+accountCity= "jaipur"
+console.log(accountPassword);
+console.table({accountId,accountEmail,accountPassword});
 
